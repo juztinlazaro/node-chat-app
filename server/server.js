5dcb8e0d352c576accd3cf1f7e293487f18c11ad
juztinlazaro <juztinlazaro@gmail.com>
2017-11-03 00:18:27 +0800

    create config

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('../.config/config.js');
 const path = require('path');
 const express = require('express');
 
@@ -6,7 +7,7 @@ const publicPath = path.join(__dirname, '../public');
 var app = express();
 app.use(express.static(publicPath));
 
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
  console.log(`Started up at port ${port}`)
 });
